1250716f83b0a3fcdbe63ac2067031f85b0e843d
mtiessler <max.tiessler@upc.edu>
2025-03-07 10:05:11 +0100

    refactored url for google

--- src/service/AuthService.ts
+++ src/service/AuthService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
+const API_BASE_URL = 'http://localhost:8080';
 
 export const login = () => {
     window.location.href = `${API_BASE_URL}/oauth2/authorization/google`;
@@ -9,7 +9,7 @@ export const login = () => {
 export const logout = async () => {
     try {
         await axios.post(`${API_BASE_URL}/logout`, {}, { withCredentials: true });
-        window.location.reload(); // Refresh page after logout
+        window.location.reload();
     } catch (error) {
         console.error('Error during logout:', error);
     }
